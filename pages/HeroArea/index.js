38ca16ae8ae2cf0b0952ce52a5782e22a38b1a94
Chase Moncrieffe Mitchem <chase.michem0855@gmail.com>
2024-07-24 17:11:49 -0500

    Working on adding an image carousel in the background

---
+++ pages/HeroArea/index.js
@@ -0,0 +1,29 @@
+import Image from "next/image";
+
+export default function HeroArea() {
+  return (
+    <>
+      <div className="hidden md:block">
+        <div
+          className="relative overflow-hidden w-[58px] h-[58px] leading-[58px] p-0"
+          style={{ width: "100%", height: "100%", margin: "0 auto" }}
+        >
+          {/* Slide 1 */}
+          <div
+            className="bg-cover bg-center transition-transform duration-[5000ms] kensburns-effect"
+            style={{ transition: "transform 5s ease-out" }}
+          >
+            <div style={{ height: "100vh", position: "relative", width: "100%"}}>
+              <Image
+                layout="fill"
+                objectFit="cover"
+                src={"/bbq1.jpg"}
+                className="whitespace-nowrap mr-0 mb-0"
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
--- pages/index.js
+++ pages/index.js
@@ -2,12 +2,14 @@ import Image from "next/image";
 import bbq from "../public/bbq1.jpg";
 import NavBar from "@/components/navbar";
 import AboutUs from "./AboutUs";
+import HeroArea from "./HeroArea";
 
 export default function Home() {
   return (
     <>
       <div className="row">
         <NavBar />
+        <HeroArea />
         <AboutUs />
       </div>
     </>
