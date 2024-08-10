dfe421d11abdd250a1f4ee728d6e1aab66beb6ee
Chase Moncrieffe Mitchem <chase.michem0855@gmail.com>
2024-08-10 15:23:59 -0500

    working on menu section

--- components/foodcard.jsx
+++ components/foodcard.jsx
@@ -1,18 +1,101 @@
-import Image from 'next/image';
-import * as React from 'react';
+import Image from "next/image";
+import * as React from "react";
 
 export default function FoodCard() {
+  const cardItems = {
+    Image: {
+      src: {},
+    },
+  };
   return (
     <>
-        <div className='flex border-gray-200 bg-pink-50'>
-            <Image src="/Brisket.webp" height={100} width={100} alt='Brisket'/>
-            <div className='bg-white border-r-[20px] pl-[75px] pr-[10px] pb-[30px] pt-[10px] shadow-custom relative'>
-                <h1>Brisket</h1>
-                <h2>Blah Blah Blah Brisket is good</h2>
-                <span className='pr-[18px]'>$40.00</span>
-                <button>Order Now</button>
-            </div>
+      <div className="flex justify-evenly flex-wrap items-center">
+        
+        <div className="flex pl-[16px] pr-[16px] mb-[16px]">
+          <div className="flex w-32 h-32 flex-shrink-0 items-center">
+            <Image
+              src="/BrisketPic.webp"
+              width={100}
+              height={100}
+              layout="responsive"
+              objectFit="cover"
+              className="rounded-md"
+              alt="Brisket"
+            />
+          </div>
+          <div className="bg-white pl-[75px] pr-[10px] pb-[30px] pt-[10px] shadow-custom relative flex flex-col justify-center">
+            <h1>Brisket</h1>
+            <h2>Blah Blah Blah Brisket is good</h2>
+            <span className="pr-[18px]">
+              $40.00 <button>Order Now</button>
+            </span>
+          </div>
         </div>
+
+        <div className="flex pl-[16px] pr-[16px] mb-[16px]">
+          <div className=" flex w-32 h-32 flex-shrink-0 items-center">
+            <Image
+              src="/bbq1.jpg"
+              layout="responsive"
+              width={100}
+              height={100}
+              objectFit="cover"
+              className="rounded-md"
+              alt="BBQ"
+            />
+          </div>
+          <div className="bg-white pl-[75px] pr-[10px] pb-[30px] pt-[10px] shadow-custom relative flex flex-col justify-center">
+            <h1>BBQ</h1>
+            <h2>Blah Blah Blah BBQ is good</h2>
+            <span className="pr-[18px]">
+              $40.00 <button>Order Now</button>
+            </span>
+          </div>
+        </div>
+
+        <div className="flex pl-[16px] pr-[16px] mb-[16px]">
+          <div className="flex w-32 h-32 flex-shrink-0 items-center">
+            <Image
+              src="/Sandwhich.webp"
+              layout="responsive"
+              width={100}
+              height={100}
+              objectFit="cover"
+              className="rounded-md"
+              alt="BBQ"
+            />
+          </div>
+          <div className="bg-white pl-[75px] pr-[10px] pb-[30px] pt-[10px] shadow-custom relative flex flex-col justify-center">
+            <h1>BBQ</h1>
+            <h2>Blah Blah Blah BBQ is good</h2>
+            <span className="pr-[18px]">
+              $40.00 <button>Order Now</button>
+            </span>
+          </div>
+        </div>
+
+        <div className="flex pl-[16px] pr-[16px] mb-[16px]">
+          <div className=" flex w-32 h-32 flex-shrink-0 items-center">
+            <Image
+              src="/Sausage.webp"
+              layout="responsive"
+              width={100}
+              height={100}
+              objectFit="cover"
+              className="rounded-md"
+              alt="BBQ"
+            />
+          </div>
+          <div className="bg-white pl-[75px] pr-[10px] pb-[30px] pt-[10px] shadow-custom relative flex flex-col justify-center">
+            <h1>BBQ</h1>
+            <h2>Blah Blah Blah BBQ is good</h2>
+            <span className="pr-[18px]">
+              $40.00 <button>Order Now</button>
+            </span>
+          </div>
+        </div>
+
+      </div>
     </>
   );
 }
--- pages/Menu/index.js
+++ pages/Menu/index.js
@@ -3,6 +3,7 @@ import FoodCard from "@/components/foodcard";
 export default function Menu() {
     return (
         <>
+        <h1 className="flex justify-center mt-[18px] mb-[18px] text-[30px] ">Menu</h1>
         <FoodCard />
         </>
     )
